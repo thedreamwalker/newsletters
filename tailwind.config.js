3120f1ae9e672d2173ace6e2ffced8798a09174e
Veronica P <vhayami@gmail.com>
2023-08-27 22:55:20 +0300

    feat: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,22 @@ export default {
     extend: {
       fontSize: {
         sub: ['13px', '16px'],
+        mainTitle: ['56px', '72px']
       },
       colors: {
         textGrayLight: '#999999',
         textGrayDark: '#666666',
+        active: '#316FEE',
+        inactive: '#EEEEEE',
+        backgroundSecond: '#F5F5F8',
+        accentLight: '#5A8CF1',
+        accentRed: '#EF3124',
       },
       fontFamily: {
         sans: ['Roboto', 'sans-serif', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
-        'cardShadow': '0px 8px 16px 0px rgba(0, 0, 0, 0.08)',
+        '2lg': '0px 8px 16px 0px rgba(0, 0, 0, 0.08)'
       }
     },
   },
