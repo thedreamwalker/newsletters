9b6c2750105abfa8bfec020949350ee4d21ca7f5
Veronica P <vhayami@gmail.com>
2023-08-27 09:38:00 +0300

    feat: add App

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderItem from './components/HeaderItem.vue'
+import NewslettresPage from './pages/NewslettresPage/NewslettresPage.vue'
+</script>
+
+<template>
+  <HeaderItem />
+
+  <NewslettresPage />
+
+</template>
