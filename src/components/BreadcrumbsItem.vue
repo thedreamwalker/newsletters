1c22fa566bcaf3cb54779259906095f251922fdf
Veronica P <vhayami@gmail.com>
2023-08-27 21:56:52 +0300

    feat: add component Breadcrumbs

---
+++ src/components/BreadcrumbsItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  breadcrumbs: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <nav class="mb-6">
+      <ul class="flex gap-2 text-sub justify-center">
+        <li :class="item.liClass" v-for="item in breadcrumbs">
+          <a :class="item.class" :href="item.url">
+            {{item.name}}
+          </a>
+        </li>
+      </ul>
+  </nav>
+</template>
