99cab4b69503952bb518ef8789f6d5233a8b1b28
Veronica P <vhayami@gmail.com>
2023-08-27 15:49:04 +0300

    feat: add component Header

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,9 @@
+<script setup>
+import IconLogo from '@/components/icons/IconLogo.vue';
+</script>
+
+<template>
+  <header class="mt-12 mb-6">
+    <IconLogo :lettersFill="'black'"></IconLogo>
+  </header>
+</template>
