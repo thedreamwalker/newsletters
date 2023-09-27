e849bf460bdcd2387bbdf37da71069b06f6ea84b
Veronica P <vhayami@gmail.com>
2023-09-27 21:39:46 +0300

    refactor: minor code change

--- src/pages/NewslettresPage/NewslettresPage.vue
+++ src/pages/NewslettresPage/NewslettresPage.vue
@@ -26,8 +26,7 @@ import { mapState, mapGetters, mapActions } from 'vuex';
   methods: {
     ...mapActions(['setSubscription']),
 
-    sendForm: function (e) {
-      e.preventDefault();
+    sendForm() {
       const obj = {email: this.email, subscriptions: this.$store.getters.subscription};
       console.log('отправлен объект');
       console.log(obj);
@@ -42,7 +41,7 @@ import { mapState, mapGetters, mapActions } from 'vuex';
   }
 }
 
-const breadcrumbsAfter = ["after:content-['→']", "after:ml-2", "after:text-textGrayLight"];
+const breadcrumbsAfter = [`after:content-['→']`, "after:ml-2", "after:text-textGrayLight"];
 const breadcrumbsDefault = ['text-textGrayLight', 'hover:text-textGrayDark', 'transition', 'ease-in-out', 'duration-300'];
 const breadcrumbsActive = ['text-accentLight', 'cursor-default'];
 
@@ -78,16 +77,16 @@ const breadcrumbs = [
     <div class="flex flex-col py-12 bg-backgroundSecond rounded-l-2xl rounded-b-2xl">
       <div class="px-12">
         <p class="mb-6 text-2xl text-center font-medium tracking-[-0.24px]">Выберите рассылки, которые подходят именно вам</p>
-        <form class="flex mb-4" @submit="sendForm">
+        <form class="flex mb-4" @submit.prevent="sendForm">
           <input class="p-4 rounded-l-xl w-full" type="email" placeholder="Электронная почта" required v-model="email">
           <div v-if="isMobile" class="bg-white rounded-r-full flex align-middle"><button class="flex justify-center items-center right-0 w-14 h-14 bg-active text-white rounded-full hover:brightness-125"><IconArrow/></button></div>
           <div v-else="isMobile" class="bg-white rounded-r-[100px]"><button class="right-0 inline-block px-8 py-4 bg-active text-white rounded-[100px] hover:brightness-125">Подписаться</button></div>
         </form>
         <div class="flex items-center gap-2">
           <SwitcherItem 
-            :width="'w-8'" 
-            :height="'h-4'" 
-            :round="'w-3 h-3'"
+            width="'w-8'" 
+            height="'h-4'" 
+            round="'w-3 h-3'"
             :isActive="allSubscription" 
             @click.prevent="changeAllSubscription(!allSubscription)"
           />
